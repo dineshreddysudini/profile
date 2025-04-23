9c9e848e7e8adbd2b6a4ca52d8b10b10fc1c9155
Dinesh Reddy Sudini <dineshreddy.sudini@lowes.com>
2025-04-23 14:19:37 -0400

    added education

--- src/Components/Education.tsx
+++ src/Components/Education.tsx
@@ -1,11 +1,27 @@
 import React from "react";
+import "../Styles/Section.css";
 
 const Education: React.FC = () => (
-  <section>
+  <section className="section">
     <h2>Education</h2>
-    <ul>
-      <li>B.Tech – [Your University], [Year]</li>
-    </ul>
+
+    <div>
+      <h3>Northwestern Polytechnic University, Fremont, CA</h3>
+      <p>
+        <strong>Master of Science in Electronic Engineering</strong>
+      </p>
+      <p>Aug 2016 – Dec 2017</p>
+    </div>
+
+    <div style={{ marginTop: "2rem" }}>
+      <h3>St. Mary’s Engineering College, Hyderabad, India</h3>
+      <p>
+        <strong>
+          Bachelor of Technology in Electronics and Communication Engineering
+        </strong>
+      </p>
+      <p>2009 – 2013 | Affiliated to JNTU Hyderabad (JNTUH)</p>
+    </div>
   </section>
 );
 
